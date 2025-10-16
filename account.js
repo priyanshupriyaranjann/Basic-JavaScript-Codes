e23d02967fc851416dc327580d221d7f58c79e59
PRIYANSHU PRIYARNJAN NAYAK <priyanshupriyaranjann@gmail.com>
2025-10-16 15:34:03 +0530

    ADD FILES

---
+++ account.js
@@ -0,0 +1,11 @@
+let a = ["HTML", "CSS", "JS"];
+console.log(a);
+
+
+let b = [12, 12, 23, 24];
+console.log(b);
+
+
+let C = ["HTML", "CSS", "JS"];
+let len = a.length;
+console.log("Array length:" + len);
